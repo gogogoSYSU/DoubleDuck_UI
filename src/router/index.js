56b9a5cdcf9539a84f3b446112480d37c3474d18
CZXHenry <812582916@qq.com>
2018-06-09 21:28:42 +0800

    Fix bugs,install axios and add ./assets/icon

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import shopHeader from '../pages/ShopHeader.vue'
 import orderPage from '../pages/OrderPage.vue'
+import storeInfoPage from '../pages/storeInfoPage.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/OrderPage',
       name: 'OrderPage',
       component: orderPage
+    },
+    {
+      path: '/storeInfoPage',
+      name: 'storeInfoPage',
+      component: storeInfoPage
     }
   ]
 })
