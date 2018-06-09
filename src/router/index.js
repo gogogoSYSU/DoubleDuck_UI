9bd92e4832b8e418b422fd3cb98a9ce3af640350
YitingKikyo <987980045@qq.com>
2018-06-09 19:00:27 +0800

    新增orger page的静态index-list实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import shopHeader from '../pages/ShopHeader.vue'
+import orderPage from '../pages/OrderPage.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: shopHeader
+    },
+    {
+      path: '/OrderPage',
+      name: 'OrderPage',
+      component: orderPage
     }
   ]
 })
